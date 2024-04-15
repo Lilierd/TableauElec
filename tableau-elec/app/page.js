b8648a950403dcb8ceec349a5e31db92e2ad28cb
Lilierd <lilierdyt@gmail.com>
2024-04-15 17:54:37 +0000

    Refactor accueil

--- tableau-elec/app/page.js
+++ tableau-elec/app/page.js
@@ -1,14 +1,40 @@
-import React from 'react';
+"use client"
+
+import React, { useRef, useState } from 'react';
 import ActiviteComponent from '@/components/activite/ActiviteComponent';
 
 export default function Home() {
   const activities = [["batteries", "Bastien", "Rémi"], ["isolement", "Rémi", "Maxime"], ["course", "Maxime", "Bastien"], ["course", "Maxime", "Bastien"], ["course", "Maxime", "Bastien"], ["course", "Maxime", "Bastien"], ["course", "Maxime", "Bastien"], ["course", "Maxime", "Bastien"], ["course", "Maxime", "Bastien"], ["course", "Maxime", "Bastien"], ["course", "Maxime", "Bastien"], ["course", "Maxime", "Bastien"]];
+  
+  const itemsRef = useRef(null);
+  const [ isMouseDown, setIsMouseDown ] = useState(false);
+  const [ startX, setStartX ] = useState(0);
+  const [ scrollLeft, setScrollLeft] = useState(false);
+
+  const handleMouseDown = (e) => {
+    setIsMouseDown(true);
+    setStartX(e.pageX - - itemsRef.current.offsetLeft);
+    setScrollLeft(itemsRef.current.scrollLeft);
+  }
+  const handleMouseLeave = () => {
+    setIsMouseDown(false);
+  }
+  const handleMouseUp = () => {
+    setIsMouseDown(false);
+  }
+  const handleMouseMove = (e) => {
+    if(!isMouseDown) return;
+    e.preventDefault();
+    const x = e.pageX - itemsRef.current.offsetLeft;
+    const walk = (x-startX)*1; // ? Adjust the speed
+      itemsRef.current.scrollLeft = scrollLeft - walk;
+  }
 
   return (
     <div>
       <div className='bg-gray-200 rounded p-2'>
         <div className='flex justify-between'><h1>Mes travaux</h1><p className='text-gray-600'>Du plus au moins récent</p></div>
-        <div className='flex overflow-x-scroll hide-scroll-bar overflow-hidden'>
+        <div ref={itemsRef} onMouseDown={handleMouseDown} onMouseLeave={handleMouseLeave} onMouseUp={handleMouseUp} onMouseMove={handleMouseMove} className='flex overflow-x-scroll hide-scroll-bar overflow-hidden'>
           <div className='flex'>
             {activities.map((activity, key) => (
               <div key={key}><div className='w-48 max-w-xs'><ActiviteComponent activite={activity} /></div></div>
@@ -21,4 +47,4 @@ export default function Home() {
       </div>
     </div>
   );
-}
+};
--- tableau-elec/components/activite/ActiviteComponent.js
+++ tableau-elec/components/activite/ActiviteComponent.js
@@ -9,15 +9,14 @@ export default function ActiviteComponent({ activite }) {
 
     return (
         <div className="inline-block my-3">
-            <Link className="divide-y divide-gray-400 border duration-300 hover:scale-110 hover:border-none transition-all border-gray-400 rounded p-4 inline-block my-2 mx-1 bg-gray-100 hover:bg-gray-300"
-                href={{
+            <div className="divide-y divide-gray-400 border duration-300 hover:scale-110 hover:border-none transition-all border-gray-400 rounded p-4 inline-block my-2 mx-1 bg-gray-100 hover:bg-gray-300">
+                <h2 className="pb-2 hover:text-orange-400 text-orange-600 transition-all"><Link href={{
                     pathname: "/activite",
                     query: { activite: activity.name},   //Remplacer par l'id dans la BDD
-                }}>
-                <h2 className="pb-2">Activité "{activity.name}"</h2>
+                }}>Activité "{activity.name}"</Link></h2>
                 <div className="py-2"><p className="font-semibold">Chargé de travaux</p><span id="CdT">{activity.CdT}</span></div>
                 <div className="pt-2"><p className="font-semibold">Exécutants</p><span id="Execs">{activity.exec}{/* Remplacer par un map de la liste de execs */}</span></div>
-            </Link>
+            </div>
         </div>
     );
 }
