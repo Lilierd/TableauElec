276053715638b9cb4ce7ba6563630030eb5ba72f
Lilierd <lilierdyt@gmail.com>
2024-06-18 22:01:09 +0000

    Custom TeButton

--- components/TeButton.js
+++ components/TeButton.js
@@ -1,6 +1,8 @@
 
-export function TeButton({ texte, onClick }) {
+export function TeButton({ texte, onClick, alert }) {
+    const fond = alert ? "bg-red-600 hover:bg-red-800 text-white" : "bg-orange-500 hover:bg-orange-800";
+
     return (
-        <button type="button" className="px-4 py-1 mr-5 rounded bg-orange-500 hover:bg-orange-800 transition-all" onClick={onClick}>{texte}</button>
+        <button type="button" className={`px-4 py-1 mr-5 rounded ${fond} transition-all`} onClick={onClick}>{texte}</button>
     );
 }
