bb676959825cda15f183b99e6fe08dc960b9c7eb
Lilierd <lilierdyt@gmail.com>
2024-06-19 11:42:30 +0000

    Header propre

--- components/header.js
+++ components/header.js
@@ -29,10 +29,7 @@ export function Header() {
   return (<>
     {userid && <header className='fixed top-0 left-0 right-0 border-b border-orange-400 p-2 flex justify-between'>
       <div className="hover:translate-x-1 hover:scale-110 duration-300 transition-all"><Link href="/">TableauElec</Link></div>
-      <div><Link className="inline-block" href="/oldPage">oldPage</Link>
-        {" | "}<Link className="inline-block" href="/oldPage/test">test</Link>
-        {" | "}<Link className="inline-block" href="/calendrier">Calendrier</Link>
-        {" | "}
+      <div>
         <div className="group inline-block"><Link href="/">Travaux<span className="group inline-block">&#129171;</span></Link>
           <div className="invisible absolute flex flex-col group-hover:visible bg-gray-300 border-l border-r border-gray-400 py-1 rounded">
             <Link className="hover:bg-gray-200 px-5" href='/activite'>Création</Link>
